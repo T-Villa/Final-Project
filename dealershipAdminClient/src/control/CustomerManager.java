a38f41ea45f14bcee28c5b73a0b783d776f1c3ad
T-Villa <thomasvillareale@gmail.com>
2025-05-05 12:33:47 -0400

    customer and sales manager built

--- dealershipAdminClient/src/control/CustomerManager.java
+++ dealershipAdminClient/src/control/CustomerManager.java
@@ -1,8 +1,29 @@
 package control;
 /**Manages customer records and search
- * methods:
- * addCustoemr(), getCustoemrHistory(), updateCustomerInfo()
- **/
-public class CustomerManager {
+**/
+import model.Customer;
+import java.util.*;
 
+public class CustomerManager {
+	private final Map<String, Customer> customers = new HashMap<>();
+	
+	public void addCustomer(Customer customer) {
+		if(customers.containsKey(customer.getID())){
+			throw new IllegalArgumentException("This customer ID already exsists");
+		}
+		else{
+			customers.put(customer.getID(),customer);
+		}
+	}
+	
+	public Customer getCustomer(String ID) {
+		return customers.get(ID);
+	}
+	
+	public List<Customer> getAllCustomers(){
+		return new ArrayList<>(customers.values());
+	}
+	public boolean removeCustomer(String ID) {
+		return customers.remove(ID) != null;
+	}
 }
--- dealershipAdminClient/src/control/SalesManager.java
+++ dealershipAdminClient/src/control/SalesManager.java
@@ -3,6 +3,31 @@ package control;
  * Methods:
  * processSale(), generateReport(), getSalesByCustomer()
  **/
-public class SalesManager {
+import java.util.*;
 
+import model.Sale;
+import model.Car;
+import model.Customer;
+
+public class SalesManager {
+	private final List<Sale> sales = new ArrayList<>();
+	
+	public void processSale(Car car, Customer customer, double price) {
+		if(car == null || customer == null) {
+			throw new IllegalArgumentException("Car and Customer must be filled out");
+		}
+		
+		Sale newSale = new Sale(car.getSKU(), customer.getID(),price);
+		sales.add(newSale);
+		
+		car.setAvailability(false);
+	}
+	
+	public List<Sale> genReport(){
+		return new ArrayList<>(sales);
+	}
+	
+	public List<Sale> getSalesByCustomer(String customerID){
+		return sales.stream().filter(sale -> sale.getCustomer().equals(customerID)).toList();
+	}
 }
