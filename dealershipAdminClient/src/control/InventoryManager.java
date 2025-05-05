2d1e665653ac13ec27e7560567a4b8ff1ec3e691
T-Villa <thomasvillareale@gmail.com>
2025-05-05 02:48:32 -0400

    added test cars

--- dealershipAdminClient/src/control/InventoryManager.java
+++ dealershipAdminClient/src/control/InventoryManager.java
@@ -10,6 +10,17 @@ import model.Car;
 public class InventoryManager {
 	private final Map<String, Car> inventory = new HashMap<>(); //would map or list be better?
 	
+	public InventoryManager(){
+    // Sample cars
+    Car car1 = new Car(2021, "Toyota", "Camry", "Black", "SE", List.of("Bluetooth", "Backup Camera"), 27000);
+    Car car2 = new Car(2020, "BMW", "X5", "White", "M Sport", List.of("Sunroof", "Leather Seats"), 55000);
+    Car car3 = new Car(2022, "Audi", "A4", "Blue", "Premium", List.of("Navigation", "Heated Seats"), 42000);
+
+    addCar(car1);
+    addCar(car2);
+    addCar(car3);
+	}
+	
 	public void addCar(Car car) {
 		String SKU = car.getSKU();
 		
@@ -26,6 +37,9 @@ public class InventoryManager {
 			return true;
 		}
 		return false;
+	}
+	public boolean removeCar(String SKU) {
+		return inventory.remove(SKU) != null;
 	}
 	
 	public List<Car> getAllCars(){
