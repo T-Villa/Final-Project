4e902381dcf17b5f374ba1a1057513415429d93c
T-Villa <thomasvillareale@gmail.com>
2025-05-06 00:14:13 -0400

    built filters and report generator

--- dealershipAdminClient/src/util/Filter.java
+++ dealershipAdminClient/src/util/Filter.java
@@ -1,6 +1,22 @@
 // methods for filtering cards by criteria
 package util;
 
-public class Filter {
+import model.Car;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Filter {
+	public static List<Car> filterCars(List<Car> cars, String make, String model, String trimLvl, String color, Integer year,Double minPrice, Double maxPrice){
+		return cars.stream()
+				.filter(car->make==null||car.getMake().equalsIgnoreCase(make))
+				.filter(car->model==null||car.getModel().equalsIgnoreCase(model))
+				.filter(car->trimLvl==null||car.getTrimLvl().equalsIgnoreCase(trimLvl))
+				.filter(car->color==null||car.getColor().equalsIgnoreCase(color))
+				.filter(car->year==null||car.getYear() == year)
+				
+				.filter(car->minPrice == null||car.getPrice() >= minPrice)
+				.filter(car->maxPrice == null||car.getPrice() <= maxPrice)
+				.collect(Collectors.toList());
+	}
 }
--- dealershipAdminClient/src/util/ReportGen.java
+++ dealershipAdminClient/src/util/ReportGen.java
@@ -1,6 +1,14 @@
 // Generate sales reports / summary
 package util;
 
-public class ReportGen {
+import model.Sale;
+import java.util.*;
 
+public class ReportGen {
+	public static double calculateTotalRev(List<Sale> sales) {
+		return sales.stream().mapToDouble(Sale::getPrice).sum();	
+		}
+	public static int countSales(List<Sale> sales) {
+		return sales.size();
+	}
 }
